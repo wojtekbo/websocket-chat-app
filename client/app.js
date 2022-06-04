7aca78a38e8390a195eceab04f43bc80dc2f7bec
Wojciech Borkowski <wojtek.borkowski@gmail.com>
2022-06-04 15:27:40 +0200

    Add Login and Lofout message

--- client/app.js
+++ client/app.js
@@ -5,7 +5,16 @@ const addMessageForm = document.querySelector('#add-messages-form');
 const userNameInput = document.querySelector('#username');
 const messageContentInput = document.querySelector('#message-content');
 
-let userName = 'Jan';
+//config
+const chatbotName = 'ChatBot';
+let userName = '';
+
+const socket = io();
+
+// nasluchiwacze
+socket.on('message', ({author, content}) => addMessage(author, content));
+socket.on('newUserLogged', userName => addMessage(chatbotName, userName + ' has joined the conversation!'));
+socket.on('userLoggedOut', userName => addMessage(chatbotName, userName + ' has left the conversation!'));
 
 const login = e => {
   e.preventDefault();
@@ -15,6 +24,7 @@ const login = e => {
     userName = userNameInput.value;
     loginForm.classList.remove('show');
     messagesSection.classList.add('show');
+    socket.emit('logged', userName);
   }
 };
 
@@ -23,6 +33,7 @@ const addMessage = (author, content) => {
   message.classList.add('message');
   message.classList.add('message--received');
   author === userName && message.classList.add('message--self');
+  author === chatbotName && message.classList.add('message--bot');
   message.innerHTML = `
   <h3 class="message__author">${userName === author ? 'You' : author}</h3>
   <div class="message__content">${content}</div>`;
@@ -31,10 +42,12 @@ const addMessage = (author, content) => {
 
 const sendMessage = e => {
   e.preventDefault();
-  if (!messageContentInput.value) {
+  let messageContent = messageContentInput.value;
+  if (!messageContent.length) {
     alert("Message can't be empty");
   } else {
-    addMessage(userName, messageContentInput.value);
+    addMessage(userName, messageContent);
+    socket.emit('message', {author: userName, content: messageContent});
     messageContentInput.value = '';
   }
 };
--- server.js
+++ server.js
@@ -1,14 +1,43 @@
 const express = require('express');
-const app = express();
+
 const path = require('path');
+const socket = require('socket.io');
+
 const messages = [];
+const users = [];
 
-app.use(express.static(__dirname + '/client'));
+const app = express();
+const server = app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
+const io = socket(server);
 
-app.get('*', (req, res) => {
-  res.sendFile(__dirname + '/client/index.html');
+io.on('connection', socket => {
+  console.log('New client! Its id – ' + socket.id);
+  socket.on('logged', user => {
+    users.push({name: user, id: socket.id});
+    console.log(`New user logged, name: ${user}, id: ${socket.id}`);
+    socket.broadcast.emit('newUserLogged', user);
+  });
+  socket.on('message', message => {
+    messages.push(message);
+    socket.broadcast.emit('message', message);
+    console.log("Oh, I've got message from " + socket.id);
+  });
+  socket.on('disconnect', user => {
+    const index = users.findIndex(user => {
+      return user.id === socket.id;
+    });
+    if (index !== -1) {
+      socket.broadcast.emit('userLoggedOut', users[index].name);
+      users.splice(index, 1);
+    }
+    console.log('Oh, socket ' + socket.id + ' has left');
+  });
+  console.log("I've added a listener on message event \n");
 });
 
-app.listen(8000, () => {
-  console.log('Server is running on port: 8000');
+app.use(express.static(__dirname + '/client'));
+app.get('*', (req, res) => {
+  res.sendFile(__dirname + '/client/index.html');
 });
